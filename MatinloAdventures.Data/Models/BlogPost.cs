180fd6ed3a2249bb18e69a7da069a4a5fcd64800
Slupov <stoyan.lupov@gmail.com>
2019-05-08 11:31:11 +0300

    [Code import]

---
+++ MatinloAdventures.Data/Models/BlogPost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MatinloAdventures.Data.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MMMM/YYYY HH:mm:ss}")]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public virtual ICollection<BlogComment> Comments { get; set; }
+    }
+}
